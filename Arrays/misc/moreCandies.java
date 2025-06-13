0719addb9f240d2f84ba347fba31b07d12a88a61
ShubhangiDimri <shubhangidimri26@gmail.com>
2025-06-13 14:13:48 +0530

    kids with the greatest no of candies

---
+++ Arrays/misc/moreCandies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> res= new ArrayList<>();
+         int max=0;
+        for(int i=0;i<candies.length;i++){
+          if(candies[i]>max){
+            max=candies[i];
+          }
+        }
+        for(int i=0;i<candies.length;i++){
+            res.add(candies[i]+extraCandies>=max);
+        }
+        return res;
+        
+    }
+}
