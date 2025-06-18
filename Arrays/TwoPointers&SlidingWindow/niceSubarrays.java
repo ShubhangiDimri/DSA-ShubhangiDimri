e8ddb585e13600c43c98f42c87f5bc66dc0159fe
ShubhangiDimri <shubhangidimri26@gmail.com>
2025-06-18 23:55:54 +0530

    Count Number of Nice Subarrays

---
+++ Arrays/TwoPointers&SlidingWindow/niceSubarrays.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        return atMost(nums,k)-atMost(nums,k-1);
+    }
+        private int atMost(int[] nums, int k){
+            int left=0,right=0,count=0,odds=0;
+            for(right=0;right<nums.length;right++){
+                if(nums[right]%2!=0){
+                    odds++;
+                }
+            while(odds>k){
+                if(nums[left]%2!=0){
+                    odds--;
+
+                }
+                left++;
+            }
+            count+=right-left+1;
+            }
+
+         return count;
+        }
+    }
