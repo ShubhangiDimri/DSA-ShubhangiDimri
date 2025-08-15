95f31b8a1266e80cc51102283f5967bac2997c9e
Shubhangi Dimri <shubhangidimri26@gmail.com>
2025-08-15 23:51:59 +0530

    subsets

---
+++ BitManipulation/subsets.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+       int subsets=1<<nums.length;
+       List<List<Integer>> ans= new ArrayList<>();
+       for(int i=0;i<=subsets-1;i++){
+        List<Integer> sub=new ArrayList<>();
+        for(int j=0;j<nums.length;j++){
+            if((i&(1<<j))!=0){
+                sub.add(nums[j]);
+            }
+        }
+        ans.add(sub);
+       } 
+     return ans;
+        
+    }
+}
